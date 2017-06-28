8a5b480c1bb3409dfc617bf7c2f68826b214bb84
odinokun <odinokun@gmail.com>
2017-06-28 16:41:27 +0300

    закончил мобильное меню и мелкие правки в адаптации главной страницы

--- src/js/modules/mobileMenu.js
+++ src/js/modules/mobileMenu.js
@@ -1,24 +1,8 @@
 module.exports = function() {
 
   // begin mobile menu
+
   $('#burger').on('click', function() {
-    // $('.sticky-header').toggleClass('active');
-      // return false;
-
-
-    // if ('#burger input:checked') {
-    //   $('.sticky-header').addClass('active');
-    // } else {
-    //   $('.sticky-header').removeClass('active');
-    // }
-
-    // if($("#burger input").prop("checked") == 'checked') { 
-    //   $('.sticky-header').addClass('active');
-    //   // return false;
-    // } else {
-    //   $('.sticky-header').removeClass('active');
-    // }
-
     if($("#burger input").is(':checked')) { 
       $('.sticky-header').addClass('active');
       $('body').addClass('no-scroll');
@@ -27,21 +11,8 @@ module.exports = function() {
       $('.sticky-header').removeClass('active');
       $('body').removeClass('no-scroll');
     }
-
-    
   });
-
   
-  // $(function() {
-
-  // });
-
-  // $(function(){     
-  //     else {
-  //           $('.sticky-header').removeClass('active');
-  //         }
-  //     return true;
-  // }
   // end   mobile menu
 
 };
